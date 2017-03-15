9ca084520829b71b8051d3eb0cf572fa0f243639
fhkhoury <f.khouryat@gmail.com>
2017-03-15 23:31:14 +0100

    Setup Firebase Crash Reporting tool =================================== - Set Crash reporting dependencies - Under the "crash" button of the 'informations' screen :      - Log and report the non-fatal error      - Develop the toast message

--- app/src/main/java/fiftyfive/and_samplefirebasegabq/Informations.java
+++ app/src/main/java/fiftyfive/and_samplefirebasegabq/Informations.java
@@ -7,27 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.crash.FirebaseCrash;
 
 import org.w3c.dom.Text;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static android.util.Log.INFO;
 import static fiftyfive.and_samplefirebasegabq.R.id.call;
 import static fiftyfive.and_samplefirebasegabq.R.id.crash;
 
 public class Informations extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
 
-        Button crash = (Button) findViewById(R.id.crash);
-        crash.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // PUT HERE THE FIREBASE CRASH CODE
-            }
-        });
 
         //App Name
         TextView appName = (TextView) findViewById(R.id.appName);
@@ -35,5 +34,23 @@ public class Informations extends AppCompatActivity {
 
         TextView udid = (TextView) findViewById(R.id.udid);
         //appName.setText(appName.getText() + " " + Context.getSystemService(Context.TELEPHONY_SERVICE).getDeviceID() );
+
+        Context context = getApplicationContext();
+        //Définition du toast
+        CharSequence text = "INFO - Non-fatal error well reported!";
+        int duration = Toast.LENGTH_LONG;
+        final Toast toast = Toast.makeText(context, text, duration);
+
+        Button crash = (Button) findViewById(R.id.crash);
+        crash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Report a non-fatal error in Crash Reporting tool
+                FirebaseCrash.logcat(INFO, "INFO", "Non-fatal error well reported"); // Logs a message that will appear in a subsequent crash report as well as in logcat
+                FirebaseCrash.report(new Exception()); // Report the exception in to the dashboard
+                toast.show();
+            }
+        });
     }
+
 }
